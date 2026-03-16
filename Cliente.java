c1a54eaed996926993fe26e287719ae6aebb1eb1
Felipe Quintero <jhoanf.quinteror@autonoma.edu.co>
2026-03-16 14:12:04 -0500

    Clase Vuelo terminada y atributos definidos

--- Cliente.java
+++ Cliente.java
@@ -1,31 +1,13 @@
 public class Cliente {
-    
-  
+    private String nombre;
+    private String cedula;
 
-    protected String nombre;
-    protected String apellido;
-    protected String telefono;
-    protected String cedula;
-    
-    
-    public Cliente(String nombre, String apellido, String telefono,String cedula ) {
+    public Cliente(String nombre, String cedula) {
         this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-        this.cedula=cedula;
+        this.cedula = cedula;
     }
 
-    public String getNombre(){return nombre;}
-    public String getApellido(){return apellido;}
-    public String getTelefono(){return telefono;}
-    public String getCedula(){return cedula;}
-     
-     
-    public String getNombreCompleto() {
-        return nombre + " " + apellido;
+    public String getNombre() {
+        return nombre;
     }
-
-    public void mostrarInfo() {
-        System.out.println("Nombre: " + getNombreCompleto());
-        System.out.println("Telefono: " + telefono);
-    }
+}
--- Vuelo.java
+++ Vuelo.java
@@ -1,4 +1,15 @@
-public class Vuelo{
+public class Vuelo {
+    private String idVuelo;
+    private String destino;
+    private double precio;
 
-  
+    public Vuelo(String idVuelo, String destino, double precio) {
+        this.idVuelo = idVuelo;
+        this.destino = destino;
+        this.precio = precio;
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("Vuelo " + idVuelo + " con destino a " + destino);
+    }
 }
